38a085c25dcdb1e37a5d2e97a7bc142b48aacca2
KurisuOfficial <151214542+KurisuOfficial@users.noreply.github.com>
2023-12-14 02:42:50 -0500

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -325,7 +325,7 @@
     const QUADTREE_MAX_POINTS = 32;
     const CELL_POINTS_MIN = 100;
     const CELL_POINTS_MAX = 250;
-    const VIRUS_POINTS = 1000;
+    const VIRUS_POINTS = 3;
     const PI_2 = Math.PI * 2;
     const SEND_254 = new Uint8Array([254, 6, 0, 0, 0]);
     const SEND_255 = new Uint8Array([255, 1, 0, 0, 0]);
