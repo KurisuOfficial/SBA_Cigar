de1ce5e6a4bbd45b51f0448c91f6e6349035a804
KurisuOfficial <151214542+KurisuOfficial@users.noreply.github.com>
2023-12-14 02:47:23 -0500

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -323,8 +323,8 @@
     const USE_HTTPS = 'https:' === window.location.protocol || window.location.hostname === 'localhost';
     const EMPTY_NAME = 'NO NAME BITCH';
     const QUADTREE_MAX_POINTS = 32;
-    const CELL_POINTS_MIN = 100;
-    const CELL_POINTS_MAX = 250;
+    const CELL_POINTS_MIN = 1;
+    const CELL_POINTS_MAX = 5;
     const VIRUS_POINTS = 3;
     const PI_2 = Math.PI * 2;
     const SEND_254 = new Uint8Array([254, 6, 0, 0, 0]);
